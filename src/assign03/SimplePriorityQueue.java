d8c7733616a12c608ac39b035c5c061fea921023
Aiden Fornalski <aidenfornalski@gmail.com>
2023-09-12 12:19:25 -0600

    Initial commit

---
+++ src/assign03/SimplePriorityQueue.java
@@ -0,0 +1,51 @@
+package assign03;
+
+import java.util.Collection;
+import java.util.NoSuchElementException;
+
+public class SimplePriorityQueue implements PriorityQueue {
+
+    public SimplePriorityQueue() {
+        
+    }
+
+    @Override
+    public Object findMax() throws NoSuchElementException {
+        return null;
+    }
+
+    @Override
+    public Object deleteMax() throws NoSuchElementException {
+        return null;
+    }
+
+    @Override
+    public void insert(Object item) {
+
+    }
+
+    @Override
+    public void insertAll(Collection coll) {
+
+    }
+
+    @Override
+    public boolean contains(Object item) {
+        return false;
+    }
+
+    @Override
+    public int size() {
+        return 0;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return false;
+    }
+
+    @Override
+    public void clear() {
+
+    }
+}

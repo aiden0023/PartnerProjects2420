5f76417e31152dc403c5bc8e02eef9f876edd3fd
Aiden Fornalski <aidenfornalski@gmail.com>
2023-09-13 16:02:42 -0600

    skeleton code

--- src/assign03/SimplePriorityQueue.java
+++ src/assign03/SimplePriorityQueue.java
@@ -1,51 +1,111 @@
 package assign03;
 
 import java.util.Collection;
+import java.util.Comparator;
 import java.util.NoSuchElementException;
 
-public class SimplePriorityQueue implements PriorityQueue {
+public class SimplePriorityQueue<E> implements PriorityQueue<E> {
+
+    private E[] queue;
+    private int size;
+    private Comparator<? super E> cmp;
 
     public SimplePriorityQueue() {
-        
+        queue = (E[]) new Object[50];
+        size = 0;
+        cmp = null;
+    }
+
+    public SimplePriorityQueue(Comparator<? super E> cmp) {
+        queue = (E[]) new Object[50];
+        size = 0;
+        this.cmp = cmp;
     }
 
     @Override
-    public Object findMax() throws NoSuchElementException {
-        return null;
+    public E findMax() throws NoSuchElementException {
+        if (this.isEmpty()) {
+            throw new NoSuchElementException();
+        } else {
+            return queue[queue.length-1];
+        }
     }
 
     @Override
-    public Object deleteMax() throws NoSuchElementException {
-        return null;
+    public E deleteMax() throws NoSuchElementException {
+        if (this.isEmpty()) {
+            throw new NoSuchElementException();
+        } else {
+            E tempMax = queue[queue.length - 1];
+            size--;
+            queue[queue.length-1] = queue[queue.length-2];
+            this.advanceQueue();
+            return tempMax;
+        }
     }
 
     @Override
-    public void insert(Object item) {
+    public void insert(E item) {
+        if (size == queue.length) {
+            this.enlargeQueue();
+        }
 
     }
 
     @Override
-    public void insertAll(Collection coll) {
+    public void insertAll(Collection<? extends E> coll) {
 
     }
 
     @Override
-    public boolean contains(Object item) {
-        return false;
+    public boolean contains(E item) {
+        return !(binarySearch(item, 0, queue.length-1) == -1);
     }
 
     @Override
     public int size() {
-        return 0;
+        return size;
     }
 
     @Override
     public boolean isEmpty() {
-        return false;
+        return this.size == 0;
     }
 
     @Override
     public void clear() {
+        queue = (E[]) new Object[20];
+        size = 0;
+    }
+
+    private int binarySearch(E target, int low, int high) {
+        if (low < high) {
+            int mid = (low/2) + (high/2);
+            int cmpOutput = compare(queue[mid], target);
+            if (cmpOutput < 0) {
+                return binarySearch(target, low, mid+1);
+            } else if (cmpOutput > 0) {
+                return binarySearch(target, mid+1, high);
+            } else {
+                return mid;
+            }
+        }
+        return -1;
+    }
+
+    private int compare(E o1, E o2) {
+        if (cmp == null) {
+            return ((Comparable<? super E>) o1).compareTo(o2);
+        } else {
+            return cmp.compare(o1, o2);
+        }
+    }
+
+    private void advanceQueue() {
+
+    }
+
+    private void enlargeQueue() {
 
     }
 }

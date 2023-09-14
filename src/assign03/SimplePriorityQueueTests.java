5ffe8fdc54de2ab5018fade079553889fd70e31e
Henry <sippelhenry4@gmail.com>
2023-09-14 12:49:37 -0600

    tests

--- src/assign03/SimplePriorityQueueTests.java
+++ src/assign03/SimplePriorityQueueTests.java
@@ -1,11 +1,36 @@
 package assign03;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
+import assign03.SimplePriorityQueue;
+import org.junit.jupiter.api.BeforeEach;
 
-public class SimplePriorityQueueTests {
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
+
+public class PriorityQueueTests {
+
+
+    private SimplePriorityQueue<Integer> priorityQueue;
+
+//    @BeforeEach
+//    void setUp() throws Exception {
+//        priorityQueue = new SimplePriorityQueue<>();
+//    }
+
 
     @Test
-    public void foo(){
+    public void testFindMax(){
+        assertNull(priorityQueue.findMax());
+        priorityQueue.insert(5);
+        assertEquals(5, priorityQueue.findMax());
+    }
 
+    @Test
+    public void testIsEmpty() {
+        assertTrue(priorityQueue.isEmpty());
+        priorityQueue.insert(5);
+        assertFalse(priorityQueue.isEmpty());
     }
 }
